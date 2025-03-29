9091a6e5f011c65ce0264400a8fcc17f3eefbed1
Pocki09 <mha5042109@gmail.com>
2025-03-29 13:29:59 +0700

    Tao CRUD cho danh sach khach hang, ViewModel cho danh sach khach hang va dang ky o ApplicationService

---
+++ QuanLyDaiLy/ViewModels/KhachHangViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QuanLyDaiLy.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using QuanLyDaiLy.Views.KhachHangViews;
+
+namespace QuanLyDaiLy.ViewModels
+{
+    public class KhachHangViewModel : INotifyPropertyChanged
+    {
+        public ICommand ThemKhachHangCommand { get; }
+        public ICommand CloseCommand { get; }
+        public ICommand CapNhatKhachHangCommand { get; }
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public KhachHangViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+
+            ThemKhachHangCommand = new RelayCommand(OpenThemKhachHang);
+            CapNhatKhachHangCommand = new RelayCommand(OpenCapNhatKhachHang);
+            CloseCommand = new RelayCommand(ExecuteClose);
+        }
+
+        public void OpenThemKhachHang()
+        {
+            var themKhachHang = _serviceProvider.GetRequiredService<ThemKhachHang>();
+            themKhachHang.ShowDialog();
+        }
+
+        public void OpenCapNhatKhachHang()
+        {
+            var capNhatKhachHang = _serviceProvider.GetRequiredService<CapNhatKhachHang>();
+            capNhatKhachHang.ShowDialog();
+        }
+
+        private void ExecuteClose()
+        {
+            Window parentWindow = Application.Current.Windows
+                    .OfType<Window>()
+                    .FirstOrDefault(w => w.IsActive);
+            parentWindow?.Close();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
