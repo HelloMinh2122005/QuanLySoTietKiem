3dbfb788dd81dc35c60df1bc63d4be7fb037c9d5
helloMinh <phandinhminh48@gmail.com>
2025-04-05 11:04:58 +0700

    KhachHang vm

---
+++ QuanLyDaiLy/Entities/Dto/DisplayKhachHangDto.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace QuanLyDaiLy.Entities.Dto
+{
+    public class DisplayKhachHangDto : INotifyPropertyChanged
+    {
+        private KhachHang _khachHang = null!;
+        public KhachHang KhachHang
+        {
+            get => _khachHang;
+            set
+            {
+                if (_khachHang != value)
+                {
+                    _khachHang = value;
+                    OnPropertyChanged();
+
+                    if (_khachHang?.DsSoTietKiem != null)
+                    {
+                        SoDu = CalculateSoDu();
+                    }
+                    else
+                    {
+                        SoDu = 0;
+                    }
+                }
+            }
+        }
+
+        private decimal _soDu;
+        public decimal SoDu
+        {
+            get => _soDu;
+            set
+            {
+                if (_soDu != value)
+                {
+                    _soDu = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private decimal CalculateSoDu()
+        {
+            if (KhachHang?.DsSoTietKiem == null || !KhachHang.DsSoTietKiem.Any())
+            {
+                return 0;
+            }
+
+            return KhachHang.DsSoTietKiem.Sum(stk => stk.SoTienGui);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- QuanLyDaiLy/ViewModels/KhachHangViewModel.cs
+++ QuanLyDaiLy/ViewModels/KhachHangViewModel.cs
@@ -6,9 +6,9 @@ using System.Windows.Input;
 using System.Windows;
 using QuanLyDaiLy.Views.KhachHangViews;
 using QuanLyDaiLy.Interfaces;
-using QuanLyDaiLy.Views;
 using QuanLyDaiLy.Entities;
 using System.Collections.ObjectModel;
+using QuanLyDaiLy.Entities.Dto;
 
 namespace QuanLyDaiLy.ViewModels
 {
@@ -18,44 +18,54 @@ namespace QuanLyDaiLy.ViewModels
         public ICommand CapNhatKhachHangCommand { get; }
         public ICommand XoaKhachHangCommand { get; }
         public ICommand CloseCommand { get; }
+        public ICommand ResetKhachHangCommand { get; }
 
         private readonly IServiceProvider _serviceProvider;
         private readonly IKhachHangRepo _khachHangRepo;
-
+        private readonly ILoaiTietKiemRepo _loaiTietKiemRepo;
 
         public KhachHangViewModel(
             IServiceProvider serviceProvider,
-            IKhachHangRepo khachHangRepo
+            IKhachHangRepo khachHangRepo,
+            ILoaiTietKiemRepo loaiTietKiemRepo
         )
         {
             _serviceProvider = serviceProvider;
             _khachHangRepo = khachHangRepo;
+            _loaiTietKiemRepo = loaiTietKiemRepo;
+
             _ = LoadData();
 
             ThemKhachHangCommand = new RelayCommand(OpenThemKhachHang);
             CapNhatKhachHangCommand = new RelayCommand(OpenCapNhatKhachHang);
             CloseCommand = new RelayCommand(ExecuteClose);
             XoaKhachHangCommand = new RelayCommand(async () => await XoaKhachHang());
+            ResetKhachHangCommand = new RelayCommand(async () => await LoadData());
         }
 
-        private KhachHang _selectedkhachHang = new();
-        public KhachHang SelectedkhachHang
+        private DisplayKhachHangDto _selectedKhachHangDto = null!;
+        public DisplayKhachHangDto SelectedKhachHangDto
         {
-            get => _selectedkhachHang;
+            get => _selectedKhachHangDto;
             set
             {
-                _selectedkhachHang = value;
-                OnPropertyChanged();
+                if (_selectedKhachHangDto != value)
+                {
+                    _selectedKhachHangDto = value;
+                    OnPropertyChanged();
+                }
             }
         }
 
-        private ObservableCollection<KhachHang> _danhSachKhachHang = [];
-        public ObservableCollection<KhachHang> DanhSachKhachHang
+        private List<KhachHang> _danhSachKhachHang = [];
+
+        private ObservableCollection<DisplayKhachHangDto> _danhSachKhachHangDto = [];
+        public ObservableCollection<DisplayKhachHangDto> DanhSachKhachHangDto
         {
-            get => _danhSachKhachHang;
+            get => _danhSachKhachHangDto;
             set
             {
-                _danhSachKhachHang = value;
+                _danhSachKhachHangDto = value;
                 OnPropertyChanged();
             }
         }
@@ -70,21 +80,96 @@ namespace QuanLyDaiLy.ViewModels
                 {
                     _searchText = value;
                     OnPropertyChanged();
-//                    FilterById();
+                    _ = FilterKhachHang();
                 }
             }
         }
 
 
+        private ObservableCollection<LoaiTietKiem> _danhSachLoaiTietKiem = [];
+        public ObservableCollection<LoaiTietKiem> DanhSachLoaiTietKiem
+        {
+            get => _danhSachLoaiTietKiem;
+            set
+            {
+                _danhSachLoaiTietKiem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private LoaiTietKiem _selectedLoaiTietKiem = null!;
+        public LoaiTietKiem SelectedLoaiTietKiem
+        {
+            get => _selectedLoaiTietKiem;
+            set
+            {
+                if (_selectedLoaiTietKiem != value)
+                {
+                    _selectedLoaiTietKiem = value;
+                    OnPropertyChanged();
+                    _ = FilterKhachHang();
+                }
+            }
+        }
+
+        private async Task FilterKhachHang()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(SearchText) && SelectedLoaiTietKiem == null)
+                {
+                    DanhSachKhachHangDto.Clear();
+                    foreach (var khachHang in _danhSachKhachHang)
+                    {
+                        DanhSachKhachHangDto.Add(new DisplayKhachHangDto
+                        {
+                            KhachHang = khachHang,
+                            SoDu = khachHang.DsSoTietKiem?.Sum(stk => stk.SoTienGui) ?? 0
+                        });
+                    }
+                }
+                else
+                {
+                    var filteredList = _danhSachKhachHang.Where(kh =>
+                        (string.IsNullOrEmpty(SearchText) || kh.TenKhachHang.Contains(SearchText)) &&
+                        (SelectedLoaiTietKiem == null || kh.DsSoTietKiem.Any(stk => stk.MaLoaiTietKiem == SelectedLoaiTietKiem.MaLoaiTietKiem))
+                    ).ToList();
+                    DanhSachKhachHangDto.Clear();
+                    foreach (var khachHang in filteredList)
+                    {
+                        DanhSachKhachHangDto.Add(new DisplayKhachHangDto
+                        {
+                            KhachHang = khachHang,
+                            SoDu = khachHang.DsSoTietKiem?.Sum(stk => stk.SoTienGui) ?? 0
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi lọc dữ liệu: {ex.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
         private async Task LoadData()
         {
             try
             {
-                var danhSachKhachHang = await _khachHangRepo.GetAll();
+                _danhSachKhachHang.Clear();
+                DanhSachKhachHangDto.Clear();
+                DanhSachLoaiTietKiem.Clear();
 
-                DanhSachKhachHang.Clear();
-                DanhSachKhachHang = [.. danhSachKhachHang];
-                
+                DanhSachLoaiTietKiem = [.. (await _loaiTietKiemRepo.GetAll())];
+
+                _danhSachKhachHang = [.. (await _khachHangRepo.GetAll())];
+                foreach (var khachHang in _danhSachKhachHang)
+                {
+                    DanhSachKhachHangDto.Add(new DisplayKhachHangDto
+                    {
+                        KhachHang = khachHang,
+                        SoDu = khachHang.DsSoTietKiem?.Sum(stk => stk.SoTienGui) ?? 0
+                    });
+                }
             }
             catch (Exception ex)
             {
@@ -103,13 +188,14 @@ namespace QuanLyDaiLy.ViewModels
 
         public void OpenCapNhatKhachHang()
         {
-            if (string.IsNullOrEmpty(SelectedkhachHang.CMND))
+            if (string.IsNullOrEmpty(SelectedKhachHangDto.KhachHang.CMND))
             {
                 MessageBox.Show("Vui lòng chọn một khách hàng để cập nhật!");
                 return;
             }
             var capNhatKhachHangViewModel = _serviceProvider.GetRequiredService<CapNhatKhachHangViewModel>();
-            capNhatKhachHangViewModel.SetData(SelectedkhachHang);
+
+            capNhatKhachHangViewModel.SetData(SelectedKhachHangDto.KhachHang);
 
 
             capNhatKhachHangViewModel.CapNhatEvent += (sender, khachHang) =>
@@ -124,14 +210,14 @@ namespace QuanLyDaiLy.ViewModels
 
         public async Task XoaKhachHang()
         {
-            if (string.IsNullOrEmpty(SelectedkhachHang.CMND))
+            if (string.IsNullOrEmpty(SelectedKhachHangDto.KhachHang.CMND))
             {
                 MessageBox.Show("Vui lòng chọn KH để xóa", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
                 return;
             }
 
             var result = MessageBox.Show(
-                $"Bạn có chắc chắn muốn xóa KH {SelectedkhachHang.TenKhachHang}?",
+                $"Bạn có chắc chắn muốn xóa Khách hàng {SelectedKhachHangDto.KhachHang.TenKhachHang}?",
                 "Xác nhận xóa",
                 MessageBoxButton.YesNo,
                 MessageBoxImage.Question);
@@ -140,7 +226,7 @@ namespace QuanLyDaiLy.ViewModels
             {
                 try
                 {
-                    await _khachHangRepo.Delete(SelectedkhachHang.CMND);
+                    await _khachHangRepo.Delete(SelectedKhachHangDto.KhachHang.CMND);
                     await LoadData();
                     MessageBox.Show("Xóa KH thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
